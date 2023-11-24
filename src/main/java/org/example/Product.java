6de9507f10182e137525c27aaab0970e8d935ebc
yaroslavrozhko <tisha7438@gmail.com>
2023-11-24 20:14:52 +0200

    final version

---
+++ src/main/java/org/example/Product.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Product {
+    private int id;
+    private String name;
+    private double price;
+
+    public Product(int id, String name, double price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+}
